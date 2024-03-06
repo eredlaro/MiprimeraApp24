0f9313c8e277ac2b40a9f94baf5b83378124be7a
eredlaro <eredlaro@gmail.com>
2024-03-06 09:46:08 -0600

    Intents y extras

--- app/src/main/java/com/example/miprimeraapp24/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp24/MainActivity.java
@@ -2,14 +2,43 @@ package com.example.miprimeraapp24;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btnIniciar;
+    EditText campo;
+    int sum = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIniciar = (Button) findViewById(R.id.init);
+        campo = (EditText)findViewById(R.id.iniciar2);
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sum = sum + 1;
+                Toast.makeText(getApplicationContext(),"Llevas  " +sum+ " clicks realizados",Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getApplicationContext(), Menu.class);
+                intent.putExtra("mensaje",campo.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
 //Comentarios
       // esto es un comentario
 
--- app/src/main/java/com/example/miprimeraapp24/Menu.java
+++ app/src/main/java/com/example/miprimeraapp24/Menu.java
@@ -2,13 +2,33 @@ package com.example.miprimeraapp24;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Menu extends AppCompatActivity {
-
+    Button regresar;
+    TextView etiqueta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        regresar = (Button) findViewById(R.id.button);
+        etiqueta = (TextView)findViewById(R.id.etiqueta);
+        Bundle extras = getIntent().getExtras();
+        if(extras!= null)
+            etiqueta.setText(extras.getString("mensaje"));
+
+
+        regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
